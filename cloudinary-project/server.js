3227ca47289b4a872a6415cc65a309c47d6e376f
Classplus <classplus@Classpluss-Ankit-Macbook-Air-Pro.local>
2025-08-28 15:13:32 +0530

    Cloudinary completed

--- cloudinary-project/server.js
+++ cloudinary-project/server.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const multer = require("multer")
+require('dotenv').config()
+const cloudinary = require("cloudinary").v2
+const fs = require('fs')
 
 const app = express()
 
@@ -14,11 +17,53 @@ const storage = multer.diskStorage({
     }
 })
 
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_SERVER_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
 const MulterUploadMiddleware = multer({storage}) 
 
-app.post("/user/upload-profile", MulterUploadMiddleware.single("profile-pic"), (req, res)=>{
-    console.log(req.file)
-    res.send("Ok")
+app.post("/user/upload-profile", MulterUploadMiddleware.single("profile-pic"), async (req, res)=>{
+    try{
+        const {mimetype : fileType, path : filePath, size : fileSize} = req.file
+        // Check for only jpeg or png
+        if(!(fileType==="image/jpeg" || fileType==="image/png")){
+            const err = new Error("File type is not supported, only png/jpeg is allowed")
+            err.statusCode = 400
+            throw err
+        }
+        // Check for file size
+        if(fileSize>=256000){
+            const err = new Error("File size should be less than 256KB")
+            err.statusCode = 400
+            throw err
+        }
+        // Upload image to cloudinary
+        const FileUploadData = await cloudinary.uploader.upload(filePath, {
+            folder : "profile_picture"
+        })
+
+        console.log(FileUploadData)
+
+        console.log(filePath)
+
+        fs.unlinkSync(filePath)
+
+        res.status(201).json({
+            success : true,
+            message : "Profile picture uploaded",
+            profileURL : FileUploadData.url
+        })
+    }catch(err){
+        console.log(err)
+        fs.unlinkSync(filePath)
+        res.status(err.statusCode || 500).json({
+            success : false,
+            message : err.message || "Internal Server Error"
+        })
+    }
 })
 
 app.listen(PORT, ()=>{
